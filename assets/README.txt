2ba97ac6779401350f655178658c8f9c2d124d19
Emir Balic <emirbalic@gmail.com>
2018-09-24 01:01:21 +0200

    All of the basic functionalities working nicely

---
+++ assets/README.txt
@@ -0,0 +1,1 @@
+# This is a very simple web app that enables an employee to manage his working day
---
+++ assets/js/todos.js
@@ -0,0 +1,63 @@
+// check off - click out
+
+// $("li").click(function() {
+//     $(this).toggleClass('completed');
+// });
+
+$('ul').on('click', 'li', function(){
+    $(this).toggleClass('completed');
+});
+
+$('ul').on('click', 'span', function(e){
+    e.stopPropagation();
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    // alert('cuc!')
+});
+
+$("input[type='text'").on('keypress', function(event){
+    if(event.which === 13){
+        var listItem = $(this).val();
+        $("ul").append("<li> <span> X </span>" + listItem + " </li>");
+        $('input').val(''); // or $(this)
+            
+        // console.log(listItem); 
+    }
+    
+    
+    
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if ($(this).css("color") === "rgb(128, 128, 128)") {
+//   //turn it black
+//   $(this).css({
+//     color: "black",
+//     textDecoration: "none"
+//   });
+// } else {
+//   //turn it gray
+//   $(this).css({
+//     color: "gray",
+//     textDecoration: "line-through"
+//   });
+// }
